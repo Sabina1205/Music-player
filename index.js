dc966910e0edd80508c5e74ef4a36af452cc855e
Sabina Nuhanovic <96692767+Sabina1205@users.noreply.github.com>
2022-06-06 18:06:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,8 @@ const musicContainer = document.getElementById('music-container');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
-
 const songTitle = document.getElementById('song');
-
 const audio = document.getElementById('audio');
-
 
 // include songs you chose for the list
 const songs = ['cant-stop', 'radioactive', 'ride'];
@@ -17,13 +14,11 @@ let songIndex = 0;
 // function from below with actual songs and song index above
 loadSong(songs[songIndex]);
 
-
 // function for song details
 function loadSong(song){
     songTitle.innerText = song;
     audio.src = `music/${song}.mp3`;
 }
-
 
 // function for play song
 function playSong() {
@@ -34,7 +29,6 @@ function playSong() {
     audio.play();
 }
 
-
 // function for pause song
 function pauseSong() {
     musicContainer.classList.remove('play');
@@ -44,20 +38,16 @@ function pauseSong() {
     audio.pause();
 }
         
-
-
 // function for previous song
 function prevSong() {
     songIndex--;
     if (songIndex < 0) {
-    songIndex = song.length - 1;
+        songIndex = song.length - 1;
     }
-
     loadSong(songs[songIndex]);
-
+    
     playSong();
-   }
-
+}
 
 // function for next song
 function nextSong() {
@@ -65,12 +55,10 @@ function nextSong() {
     if (songIndex > songTitle.length -1) {
             songIndex = 0;
     }
-
     loadSong(songs[songIndex]);
 
     playSong();
-    }
-
+}
 
 // event listener for play pause
 playBtn.addEventListener('click', () => {
@@ -81,20 +69,17 @@ if (isPlaying) {
     } else {
         playSong();
     }
-    });
+});
 
-   
 // event listener for buttons previous and next
 prevBtn.addEventListener('click', prevSong);
 nextBtn.addEventListener('click', nextSong);
-
 
 // next song autoplay
 audio.addEventListener('ended', () => {
     songIndex++;
     if (songIndex >= song.length) {
         songIndex = 0;
-       
     }
     loadSong(songs[songIndex]);
 
